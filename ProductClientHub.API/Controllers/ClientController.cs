08785a60f6365a0dc25b82193da4d7445c7743b4
Ryan de Menezes <menezesryan1010@gmail.com>
2024-12-11 01:43:15 -0300

    first commit

---
+++ ProductClientHub.API/Controllers/ClientController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ClientResponse), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] ClientRequest request)
+        {
+            return Created();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] ClientRequest request)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            return Ok();
+        }
+    }
+}
